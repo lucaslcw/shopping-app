67a34547f708c379f530a8ca5afc234b6a820da4
Lucas Cavalcante <cavalcantelucasx@gmail.com>
2023-08-10 07:37:15 -0300

    Initial Commit

---
+++ shopping-app/shopping_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  shopping_appApp.swift
+//  shopping-app
+//
+//  Created by LUCAS on 10/08/23.
+//
+
+import SwiftUI
+
+@main
+struct shopping_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
